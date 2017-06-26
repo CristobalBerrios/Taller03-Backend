a5f0b62fa0f6cf96c7d9147fe0d86e4fb38d3454
Xiterss <c.berrios01@ufromail.cl>
2017-06-26 01:52:55 -0400

    Primer commit

---
+++ app/Http/Controllers/LibroController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Libro;
+
+use App\Autor;
+
+use App\Genero;
+
+class LibroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $libros = Libro::all();
+        $datos = array();
+        $cont = 0;
+
+        foreach ($libros as $libro) {
+            $autor = Autor::find($libro->autor_id);
+            $genero = Genero::find($libro->genero_id);
+
+            $datos[$cont]['id'] = $libro->id;
+            $datos[$cont]['titulo'] = $libro->titulo;
+            $datos[$cont]['autor'] = $autor;
+            $datos[$cont]['genero'] = $genero;
+
+            $cont++;
+        }
+
+        return $datos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/user', function (Request $request) {
+	return $request->user();
+})->middleware('auth:api');
+
+Route::group(['middleware'=>['cors']],function(){
+
+	Route::resource('generos', 'GeneroController');
+
+	Route::resource('usuarios', 'UsuarioController');
+
+	Route::resource('autor', 'AutorController');
+
+	Route::resource('ejemplar', 'EjemplarController');
+
+	Route::resource('libros', 'LibroController');
+});
+
+
+
